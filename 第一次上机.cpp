716a539b8b00d905d63bcfb37680bb3ec83e3b6b
PrpgramTraveler <2115655391@qq.com>
2021-04-27 20:36:52 +0800

    2021-04-27

--- 第一次上机.cpp
+++ 第一次上机.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 string token;
 double num = 1;
+int len = 0;
 
-bool isnum(string s) {
+bool isnum(char s) {
 	stringstream sin(s);
 	double t;
 	char p;
@@ -21,74 +22,140 @@ bool isnum(string s) {
 	return true;
 }
 
-int scanner(int i, vector<string>& s) {
+int scanner(int i, string s) {
 	int res = 0;
+	int index = i;
+	len = 0;
+	token = "";
+	if (s[index] - 'a' >= 0 && s[index] - 'a' <= 25) {
+		while (index < s.size() && s[index] - 'a' >= 0 && s[index] - 'a' <= 25) {
+			if (s[index] == ' ') break;
+			token += s[index];
+			index++;
+			len++;
+		}
+		if (token == "begin") return 1;
+		else if (token == "if") return 2;
+		else if (token == "then") return 3;
+		else if (token == "while") return 4;
+		else if (token == "do") return 5;
+		else if (token == "end") return 6;
+		else return 10;
+	}
+	if (s[index] == ' ') { 
+		cout << "space" << endl;
+		len ++;
+		return -12;
+	}
+	/*if (s[i] == 'b') {
+		if (s[i + 1] == 'e' && s[i + 2] == 'g' && s[i + 3] == 'i' && s[i + 4] == 'n' && (s[i + 5] - 'a' < 0 && s[i + 5] - 'a'  >25 )) {
+			res = 1;
+			//token = "begin";
+		}
+		else {
+			res = 10;
+			token += s[i];
+		}
 
-	if (s[i] == "begin") {
-		res = 1;
-
 	}
-	else if (s[i] == "if") {
-		res = 2;
+	else if (s[i] == 'i') {
+		if (s[i + 1] == 'f' &&(s[i + 2] - 'a' < 0 && s[i + 2] - 'a' > 25)) {
+			
+			res = 2;
+			//token = "if";
+		}
+		else {
+			res = 10;
+			token += s[i];
+		}
 	}
-	else if (s[i] == "then") {
-		res = 3;
+	else if (s[i] == 't') {
+		if (s[i + 1] == 'h' && s[i + 2] == 'e' && s[i + 3] == 'n' && (s[i + 4] - 'a' < 0 && s[i + 4] - 'a' > 25)) {
+			res = 3;
+			//token = "then";
+		}
+		else {
+			res = 10;
+			token = s[i];
+		}
 	}
-	else if (s[i] == "while") {
-		res = 4;
+	else if (s[i] == 'w') {
+		if (s[i + 1] == 'h' && s[i + 2] == 'i' && s[i + 3] == 'l' && s[i + 4] == 'e' && (s[i + 5] - 'a' < 0 && s[i + 5] - 'a' > 25)) {
+			res = 4;
+			//token = "while";
+		}
+		else {
+			res = 10;
+			token = s[i];
+		}
 	}
-	else if (s[i] == "do") {
-		res = 5;
+	else if (s[i] == 'd') {
+		if (s[i + 1] == 'o' && (s[i + 2] - 'a' < 0 && s[i + 2] - 'a' > 25)) {
+			res = 5;
+			//token = "do";
+		}
+		else {
+			res = 10;
+			token = s[i];
+		}
 	}
-	else if (s[i] == "end") {
-		res = 6;
+	else if (s[i] == 'e') {
+		if (s[i + 1] == 'n' && s[i + 2] == 'd' && (s[i + 3] - 'a' < 0 && s[i + 3] - 'a' > 25)) {
+			res = 6;
+			//token = "end";
+		}
+		else {
+			res = 10;
+			token = s[i];
+		}
 	}
-	else if (s[i] == "+") {
+	else if (s[i] == '+') {
 		res = 13;
 	}
-	else if (s[i] == "-") {
+	else if (s[i] == '-') {
 		res = 14;
 	}
-	else if (s[i] == "*") {
+	else if (s[i] == '*') {
 		res = 15;
 	}
-	else if (s[i] == "/") {
+	else if (s[i] == '/') {
 		res = 16;
 	}
-	else if (s[i] == ":") {
-		res = 17;
+	else if (s[i] == ':') {
+		if (s[i + 1] == '=')
+			res = 18;
+		else
+			res = 17;
+
 	}
-	else if (s[i] == ":=") {
-		res = 18;
+	
+	else if (s[i] == '<') {
+		if (s[i + 1] == '>')
+			res = 21;
+		else if (s[i + 1] == '=')
+			res = 22;
+		else
+			res = 20;
 	}
-	else if (s[i] == "<") {
-		res = 20;
+	else if (s[i] == '>') {
+		if (s[i + 1] == '=')
+			res = 24;
+		else 
+			res = 23;
 	}
-	else if (s[i] == "<>") {
-		res = 21;
-	}
-	else if (s[i] == "<=") {
-		res = 22;
-	}
-	else if (s[i] == ">") {
-		res = 23;
-	}
-	else if (s[i] == ">=") {
-		res = 24;
-	}
-	else if (s[i] == "=") {
+	else if (s[i] == '=') {
 		res = 25;
 	}
-	else if (s[i] == ";") {
+	else if (s[i] == ';') {
 		res = 26;
 	}
-	else if (s[i] == "(") {
+	else if (s[i] == '(') {
 		res = 27;
 	}
-	else if (s[i] == ")") {
+	else if (s[i] == ')') {
 		res = 28;
 	}
-	else if (s[i] == "#") {
+	else if (s[i] == '#') {
 		res = 0;
 	}
 	else if (isnum(s[i])) {
@@ -97,7 +164,7 @@ int scanner(int i, vector<string>& s) {
 	else {
 		res = 10;
 		token = s[i];
-	}
+	}*/
 	//cout << res << endl;
 	return res;
 }
@@ -107,15 +174,25 @@ int main() {
 
 	int i = 0;
 
-	vector<string> str;
-	string ch;
+	cout << "请输入：" << endl;
+	/*vector<char> str;
+	char ch;
+	cout << "请输入：" << endl;
 	while (cin >> ch) {
 		str.push_back(ch);
-		if (ch == "#")
+		if (ch == '#')
 			break;
-	}
+	}*/
+	string str;
+
+	getline(cin, str);
+
+	cout << str << endl;
+
 	int syn;
 	int index = 0;
+	cout << endl;
+	cout << "词法分析结果："<<endl;
 	do {
 		syn = scanner(index, str);
 		//cout << "syn:" << syn << endl;
@@ -198,10 +275,28 @@ int main() {
 		default:
 			break;
 		}
-		index++;
+		/*if (syn == 1 || syn == 4) {
+			index += 5;
+		}
+		else if (syn == 2 || syn == 5 || syn == 21 || syn == 24 || syn == 18 || syn == 22 || syn == 24) {
+			index += 2;
+		}
+		else if (syn == 3) {
+			index += 4;
+		}
+		else if (syn == 6) {
+			index += 3;
+		}
+		else {
+			index++;
+		}*/
+		index += len;
+		cout << "index:" << index << endl;
 	} while (syn != 0);
-
+	cout << endl;
+	cout << "最初的输入:" << endl;
 	for (int i = 0; i < str.size(); i ++)
-		cout<<str[i];
+		//cout<<str[i];
+	cout << endl;
 	return 0;
 }
